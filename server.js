043d8659390e55f8faaddadf4cf92a10e2d829c1
abangdsm <id.reversal@gmail.com>
2023-02-20 14:26:11 +0700

    Menambahkan modul server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { json } = require("express/lib/response");
+const mysql = require("mysql");
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: "localhost",
+    database: "ucapan",
+    user: "root",
+    password: ""
+});
+
+db.connect((err) => {
+    if (err) throw err
+    console.log('Database Connected');
+
+    const sql = "SELECT * FROM tbucapan"
+    db.query(sql, (err, result) => {
+        const users = JSON.parse(JSON.stringify(result));
+        console.log('Hasil database -> ', users);
+        app.get("/", (req, res) =>{
+            res.send(users);
+        });
+    });
+
+    
+});
+
+app.listen(8000, () => {
+    console.log('server ready...');
+})
